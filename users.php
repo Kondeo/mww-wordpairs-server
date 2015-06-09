7c7016865520ea4b06e5f30a84ef22955b999e87
Julian Poyourow <julianpoyo@gmail.com>
2015-06-08 19:15:18 -0700

    Removed echo of meta tag, no longer necessary

--- users.php
+++ users.php
@@ -306,7 +306,6 @@ function userLogin() {
 
 function getPage($page, $token) {
     $request = Slim::getInstance()->request()->get();
-    echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> ';
 
     $sql = "SELECT
 
